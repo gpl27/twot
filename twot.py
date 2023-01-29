a87a7950f2796a4742840a0d525a818b6883f628
Gustavo Lacroix <gustavolacroix@gmail.com>
2023-01-29 17:34:35 -0300

    Added reply, retweet and quote retweet

--- twot.py
+++ twot.py
@@ -5,11 +5,13 @@
 """
 
 from selenium import webdriver
+from selenium.common.exceptions import *
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.relative_locator import locate_with
 
 
 class TwitterAPI:
@@ -20,6 +22,7 @@ class TwitterAPI:
     #   * add error handling to login (wrong user|pwd)
     #   * add error handling to undefined tweet_ids
     #   * add status() function
+    #   * add logger
 
     def __init__(self, username, password):
         self.username = username
@@ -30,7 +33,18 @@ class TwitterAPI:
         self.driver = webdriver.Chrome(service=service)
         self.wait = WebDriverWait(self.driver, timeout=5)
 
+
+    def _post(self, message):
+        text_input = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@aria-label="Tweet text"]'))
+        text_input.click()
+        text_input.send_keys(message)
+        tweet_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@data-testid="tweetButton"]'))
+        tweet_button.click()
+        alert = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@role="alert" and @data-testid="toast"]'))
+        tweet_link = alert.find_element(By.TAG_NAME, 'a').get_attribute("href")
+        return tweet_link
     
+
     def login(self):
         if (self.logged):
             print(f"[TwitterAPI:login] Already logged in as {self.username}")
@@ -52,7 +66,7 @@ class TwitterAPI:
         self.driver.get("https://twitter.com/logout")
         logout_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@data-testid="confirmationSheetConfirm"]'))
         logout_button.click()
-        self.wait.until(EC.title_contains("Explore"))
+        self.wait.until(EC.url_contains("?"))
         self.logged = False
         return True
 
@@ -62,16 +76,24 @@ class TwitterAPI:
             print(f"[TwitterAPI:post_tweet] Must log in first")
             return ""
         self.driver.get("https://twitter.com/compose/tweet")
-        text_input = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@aria-label="Tweet text"]'))
-        text_input.click()
-        text_input.send_keys(message)
-        tweet_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@data-testid="tweetButton"]'))
-        tweet_button.click()
-        alert = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@role="alert" and @data-testid="toast"]'))
-        tweet_link = alert.find_element(By.TAG_NAME, 'a').get_attribute("href")
-        return tweet_link
-
+        return self._post(message)
+        
     
+    def reply_to_tweet(self, tweet_id, message):
+        if not self.logged:
+            print(f"[TwitterAPI:reply_to_tweet] Must log in first")
+            return ""
+        tweet_url = tweet_id_to_url(tweet_id)
+        self.driver.get(tweet_url)
+        reply_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@aria-label="Reply"]'))
+        try:
+            reply_button.click()
+        except:
+            return self.reply_to_tweet(tweet_id, message)
+        
+        return self._post(message)
+
+
     def like_tweet(self, tweet_id):
         if not self.logged:
             print(f"[TwitterAPI:like_tweet] Must log in first")
@@ -79,24 +101,59 @@ class TwitterAPI:
         tweet_url = tweet_id_to_url(tweet_id)
         self.driver.get(tweet_url)
         like_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@aria-label="Like" or @aria-label="Liked"]'))
-        like_button.click()
+        try:
+            like_button.click()
+        except:
+            return self.like_tweet(tweet_id)
         like_label = like_button.get_attribute("aria-label")
         like_status = True if like_label == "Liked" else False
         return like_status
 
-
-    # def reply_to_tweet(self, tweet_id, message):
-    #     tweet_url = tweet_id_to_url(tweet_id)
-    #     self.driver.get(tweet_url)
-
     
-    # def retweet(self, tweet_id):
-    #     tweet_url = tweet_id_to_url(tweet_id)
-    #     self.driver.get(tweet_url)
+    def retweet(self, tweet_id):
+        if not self.logged:
+            print(f"[TwitterAPI:like_tweet] Must log in first")
+            return None
+        tweet_url = tweet_id_to_url(tweet_id)
+        self.driver.get(tweet_url)
+        retweet_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@aria-label="Retweet" or @aria-label="Retweeted"]'))
+        try:
+            retweet_button.click()
+        except:
+            return self.retweet(tweet_id)
+        confirm_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@data-testid="retweetConfirm" or @data-testid="unretweetConfirm"]'))
+        self.wait.until(EC.element_to_be_clickable(confirm_button))
+        confirm_button.click()
+        retweet_label = retweet_button.get_attribute("aria-label")
+        retweet_status = True if retweet_label == "Retweeted" else False
+        return retweet_status
+
+
+    def quote_retweet(self, tweet_id, message):
+        if not self.logged:
+            print(f"[TwitterAPI:quote_retweet] Must log in first")
+            return None
+        tweet_url = tweet_id_to_url(tweet_id)
+        self.driver.get(tweet_url)
+        retweet_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@aria-label="Retweet" or @aria-label="Retweeted"]'))
+        try:
+            retweet_button.click()
+        except:
+            return self.quote_retweet(tweet_id, message)
+        confirm_button = self.wait.until(lambda d: d.find_element(By.XPATH, '//div[@data-testid="retweetConfirm" or @data-testid="unretweetConfirm"]'))
+        qrt_locator = locate_with(By.TAG_NAME, 'div').below(confirm_button)
+        qrt = self.driver.find_element(qrt_locator)
+        self.wait.until(EC.element_to_be_clickable(qrt))
+        qrt.click()
+        return self._post(message)
 
 
     def quit(self):
         self.driver.quit()
+
+    def shutdown(self):
+        self.logout()
+        self.quit()
 
 
 

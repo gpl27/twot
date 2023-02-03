55e4169552399f9a91ce8ff2b1f0c3044d4bf7e1
Gustavo Lacroix <gustavolacroix@gmail.com>
2023-02-03 20:36:11 -0300

    Improved sample script

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env
 
 """
-    Sample script of twot usage
-    
-    Logs in to an account, gets latest tweets and engages with them
+Sample script of twot usage
 
+Logs in to an account, tweets, quote retweets, likes, and follows
 
-    gpl27
+USAGE:
+$ ./main.py [username] [password]
+
+Author: gpl27
 """
 
 import logging
 import time
 from twot import *
+from sys import argv
 
 # Sets ups console and file logging
-
-# Get twots logger
 logger = logging.getLogger('twot')
 logger.setLevel(logging.DEBUG)
-
-# Add a console handler
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 ch_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(ch_format)
 logger.addHandler(ch)
-
-# Add a file handler
 fh = logging.FileHandler('main.log', mode='w')
 fh.setLevel(logging.DEBUG)
 fh_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -34,14 +31,26 @@ fh.setFormatter(fh_format)
 logger.addHandler(fh)
 
 
-# Runs code using the TwitterAPI
-api = TwitterAPI('TheTwotBot', 'twotthetwitterbot')
+def main(username, password):
+    # Runs code using the TwitterAPI
+    api = TwitterAPI(username,password)
 
-api.login()
-my_tweet = api.post_tweet(time.strftime('%H:%M:%S', time.localtime()))
-my_tweet += '0'
+    api.login()
+    message = f"{time.strftime('%H:%M:%S', time.localtime())} - This is a sample tweet that was posted using twot-TheTwitterAPI"
+    my_tweet = api.post_tweet(message)
 
-api.like_tweet(my_tweet)
+    api.like_tweet(my_tweet)
 
-api.logout()
-api.quit()
+    message = f"{time.strftime('%H:%M:%S', time.localtime())} - This is a sample quote retweet"
+    my_tweet = api.quote_retweet(my_tweet, message)
+
+    api.like_tweet(my_tweet)
+
+    api.follow('TheTwotBot')
+
+    api.logout()
+    api.quit()
+
+
+if __name__ == "__main__":
+    main(argv[1], argv[2])
